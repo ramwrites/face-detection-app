e8c81fa7d2ead5bbea42de8473d69a3c32bf5c58
ramwrites <ramalagonda123@gmail.com>
2025-06-04 15:42:42 +0530

    second commit

--- app.py
+++ app.py
@@ -1,58 +1,56 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import numpy as np
 import cv2
 import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
 
 facetrack = tf.keras.models.load_model('model/facetracker.h5')
 
 app = Flask(__name__)
-cap = cv2.VideoCapture(0)
-
-def gen_frames():
-    while True:
-        success, frame = cap.read()
-
-        if not success:
-            break
-        else:
-            rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        resized = tf.image.resize(rgb, (120,120))
-        
-        yhat = facetrack.predict(np.expand_dims(resized/255,0))
-        sample_coords = yhat[1][0]
-        
-        if yhat[0] > 0.5: 
-            image_width, image_height = 640, 480
-            x_center, y_center, box_width, box_height = sample_coords 
-            
-            x_center_pixel = x_center * image_width
-            y_center_pixel = y_center * image_height
-            width_pixel = box_width * image_width
-            height_pixel = box_height * image_height
-            
-            # Compute xmin, ymin, xmax, ymax
-            x_min = int(x_center_pixel - width_pixel / 2)
-            y_min = int(y_center_pixel - height_pixel / 2)
-            x_max = int(x_center_pixel + width_pixel / 2)
-            y_max = int(y_center_pixel + height_pixel / 2)
-            # Controls the main rectangle
-            cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), 
-                                (0,255,0), 2)
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-
-        yield(b'--frame\r\n'
-              b'content-type: image/jepg\r\n\r\n' + frame +
-              b'\r\n')
+socketio = SocketIO(app)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-@app.route('/video')
-def video():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@socketio.on('message')
+def handle_message(frame):
+    image = np.frombuffer(frame, dtype= np.uint8)
+    img = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    
+    resized = tf.image.resize(img, (120,120))
+    
+    yhat = facetrack.predict(np.expand_dims(resized/255,0))
+    sample_coords = yhat[1][0]
+
+    if yhat[0] > 0.5: 
+        image_width, image_height = 640, 480
+        x_center, y_center, box_width, box_height = sample_coords 
+        
+        x_center_pixel = x_center * image_width
+        y_center_pixel = y_center * image_height
+        width_pixel = box_width * image_width
+        height_pixel = box_height * image_height
+        
+        # Compute xmin, ymin, xmax, ymax
+        x_min = int(x_center_pixel - width_pixel / 2)
+        y_min = int(y_center_pixel - height_pixel / 2)
+        x_max = int(x_center_pixel + width_pixel / 2)
+        y_max = int(y_center_pixel + height_pixel / 2)
+        # Controls the main rectangle
+        cv2.rectangle(img, (x_min, y_min), (x_max, y_max), 
+                            (0,255,0), 2)
+
+    _, buffer = cv2.imencode('.jpg', img)
+    socketio.emit('response', buffer.tobytes())
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    socketio.run(app, debug=True)
