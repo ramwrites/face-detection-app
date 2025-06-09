90143f0a6de6f13efb0081177c2807595bc82d4a
ramwrites <ramalagonda123@gmail.com>
2025-06-09 12:59:00 +0530

    updated

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def handle_message(frame):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=10000, debug=True)
 
